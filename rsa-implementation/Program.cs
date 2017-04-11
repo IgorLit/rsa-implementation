daa74ea80c6678bf0799c251e5780c15854a2b01
dzianis.buividovich <dzianis.buividovich@itechart-group.com>
2017-04-11 18:11:29 +0300

    version 1.0.0

---
+++ rsa-implementation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsa_implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RSA rsa = new RSA();
+            Console.WriteLine(rsa.getEncriptedText("hello"));
+
+            Console.WriteLine(rsa.getDecryptedText(rsa.getEncriptedText("hello")));
+        }
+    }
+}
---
+++ rsa-implementation/RSA.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsa_implementation
+{
+    class RSA
+    {
+
+        private static List<int> arr;
+        private static Random rnd;
+        private BigInteger e;
+        private BigInteger d;
+        private BigInteger n;
+
+        public RSA()
+        {
+            arr = initPrimeNum();
+            rnd = new Random();
+            getPQ();
+        }
+
+        private List<int> initPrimeNum()
+        {
+            List<int> arr = new List<int>();
+            int low = 10000, high = 20000, i, flag;
+            while (low < high)
+            {
+                flag = 0;
+
+                for (i = 2; i <= low / 2; ++i)
+                {
+                    if (low % i == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+                if (flag == 0)
+                    arr.Add(low);
+
+                ++low;
+            }
+            return arr;
+        }
+
+        private void getPrimeNum(Random rnd, List<int> arr, out int n1, out int n2)
+        {
+            n1 = arr[rnd.Next(arr.Count)];
+            n2 = arr[rnd.Next(arr.Count)];
+        }
+
+        private BigInteger getN(int p, int q)
+        {
+            return p * q;
+        }
+
+        private BigInteger getFi(int p, int q)
+        {
+            return (p - 1) * (q - 1);
+        }
+
+        private BigInteger NOD(BigInteger a, BigInteger b)
+        {
+            if (a == b)
+                return a;
+            else
+                if (a > b)
+                return NOD(a - b, b);
+            else
+                return NOD(b - a, a);
+        }
+
+        private BigInteger getPrime(Random rnd, BigInteger fi)
+        {
+            BigInteger e = rnd.Next(1, Math.Abs((int)fi));
+            do
+            {
+                if (NOD(e, fi) == 1) break;
+                else e++;
+            } while (true);
+            if (e >= fi)
+            {
+                e--;
+                do
+                {
+                    if (NOD(e, fi) == 1) break;
+                    else e--;
+                } while (true);
+            }
+            return e;
+        }
+
+        private BigInteger getD(BigInteger e, BigInteger fhi)
+        {
+            BigInteger i = fhi, v = 0, d = 1;
+            while (e > 0)
+            {
+                BigInteger t = i / e, x = e;
+                e = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= fhi;
+            if (v < 0) v = (v + fhi) % fhi;
+            return v;
+        }
+
+        private string encript(int symbol, BigInteger keyE, BigInteger keyN)
+        {
+            return BigInteger.ModPow(symbol, keyE, keyN).ToString();
+        }
+
+        private int decript(int symbol, BigInteger keyD, BigInteger keyN)
+        {
+            return (int)(BigInteger.ModPow(symbol, keyD, keyN));
+        }
+
+        private string textEncript(string text, BigInteger e, BigInteger n)
+        {
+            string eText = "";
+            foreach (var symbol in text)
+            {
+                eText += encript(symbol, e, n) + '|';
+            }
+            return eText;
+        }
+
+        private string textDecript(string text, BigInteger d, BigInteger n)
+        {
+            string[] arr = text.Split('|');
+            string dText = "";
+            foreach (var symbol in arr)
+            {
+                if (symbol != String.Empty)
+                    dText += Convert.ToChar(decript(int.Parse(symbol), d, n));
+            }
+            return dText;
+        }
+
+        public string getEncriptedText(string originalText)
+        {
+            return textEncript(originalText, this.e, this.n);
+        }
+
+        public string getDecryptedText(string encryptedText)
+        {
+            return textDecript(encryptedText, this.d, this.n);
+
+        }
+
+        private void getPQ()
+        {
+            int q;
+            int p;
+            getPrimeNum(rnd, arr, out q, out p);
+            BigInteger fi = getFi(p, q);
+            this.n = getN(p, q);
+            this.e = getPrime(rnd, fi);
+            this.d = getD(e, fi);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getPQ();
+        }
+
+    }
+
+}
